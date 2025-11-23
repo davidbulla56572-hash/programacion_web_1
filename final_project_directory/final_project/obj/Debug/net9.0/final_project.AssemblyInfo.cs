1998271a2c2c9c26407fe18814fbbea29f882b45
david.bulla56572-hash <david.bulla56572@ucaldas.edu.co>
2025-11-22 19:29:18 -0500

    agrega lo local

--- final_project_directory/final_project/obj/Debug/net9.0/final_project.AssemblyInfo.cs
+++ final_project_directory/final_project/obj/Debug/net9.0/final_project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("final_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8f3232a018a706ccf6c7b293d752628b9d30568")]
 [assembly: System.Reflection.AssemblyProductAttribute("final_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("final_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
